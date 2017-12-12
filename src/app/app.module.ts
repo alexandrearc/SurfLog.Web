8ee7e40d8b10fc76245dee79851f680890e88b9c
Alexandre Campinho <alexandrearc@gmail.com>
2017-12-12 22:41:07 +1300

    Update to angular 5.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -40,6 +41,7 @@ import { APP_PROVIDERS } from './app.providers';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     HttpModule,
     AppRoutingModule,
     BrowserAnimationsModule,
